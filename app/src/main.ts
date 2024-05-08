ac01ff93c92ce7895b8d76b90f040b1f1b3e061c
Sean DePottey <sean.depottey@valtech.com>
2024-05-08 13:11:43 -0400

    Service worker start integrating blob to ensure no external files needed

--- app/src/main.ts
+++ app/src/main.ts
@@ -8,14 +8,35 @@ if (testBtn) {
   })
 }
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then((reg) => {
-      if (reg.active) {
-        console.log('service worker registered');
-      }
-    })
-    .catch((err) => console.error(err));
-} else {
-  console.log('not in nav')
+// #region service worker
+function interceptRequest() {
+  
 }
+
+const serviceWorkerCode = `self.addEventListener('install', () => {
+  console.log('install');
+});
+
+self.addEventListener('fetch', function(event) { interceptRequest });
+`
+
+const serviceWorkerBlob = new Blob([serviceWorkerCode], { type: 'application/javascript' });
+const serviceWorkerBlobUrl = URL.createObjectURL(serviceWorkerBlob);
+
+const registerServiceWorker = async (blobUrl: string) => {
+  if ('serviceWorker' in navigator) {
+      // navigator.serviceWorker.register(blobUrl)
+      navigator.serviceWorker.register('./sw')
+      .then((reg) => {
+        if (reg.active) {
+          console.log('service worker registered');
+        }
+      })
+      .catch((err) => console.error(err));
+  } else {
+    console.log('not in nav')
+  }
+}
+
+registerServiceWorker(serviceWorkerBlobUrl);
+// #endregion service worker

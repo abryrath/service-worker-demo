5107b3340a1916475d84ae7cec3bb7f66b662129
Sean DePottey <sean.depottey@valtech.com>
2024-05-14 09:17:25 -0400

    Updates

--- app/public/sw.js
+++ app/public/sw.js
@@ -38,70 +38,64 @@
  * Update the 4 video asset links to the non-interactive 
  * hosting for the videos.
 */
-self.addEventListener('fetch', function(event) {
-  const url = event.request.url;
-  console.log(`[sw] detected url: ${url}`);
-  event.respondWith(
-    fetch(event.request)
-      .then((resp) => {
-        if (resp.ok && resp.body) {
-          const targetBlockId = 73417; // JLO
-          const replacementAssets = { // ED assets
-            dolbyOffPortraitAsset: {
-              hlsVideoUrl: "https://experience-media.dolby.com/Dolby.com/20231227/Love_More/Ed_Sheeran/SDRS/Portrait/unencrypted/hls/LM_ES-Destination_SDR-9x16_A-Binaural_vs_Stereo_SDRS_manifest-priority.m3u8",
-              mpegDashVideoUrl: "https://experience-media.dolby.com/Dolby.com/20231227/Love_More/Ed_Sheeran/SDRS/Portrait/unencrypted/dash/LM_ES-Destination_SDR-9x16_A-Binaural_vs_Stereo_SDRS_manifest-priority.mpd"
-            },
-            dolbyOffLandscapeAsset: {
-              hlsVideoUrl: "https://experience-media.dolby.com/Dolby.com/20231227/Love_More/Ed_Sheeran/SDRS/Landscape/unencrypted/hls/LM_ES-Destination_SDR_A-Binaural_vs_Stereo_SDRS_manifest-priority.m3u8",
-              mpegDashVideoUrl: "https://experience-media.dolby.com/Dolby.com/20231227/Love_More/Ed_Sheeran/SDRS/Landscape/unencrypted/dash/LM_ES-Destination_SDR_A-Binaural_vs_Stereo_SDRS_manifest-priority.mpd"
-            }
-          }
+fetch(event.request)
+.then((resp) => {
+  if (resp.ok && resp.body) {
+    const targetBlockId = 73417; // JLO
+    const replacementAssets = { // ED assets
+      dolbyOffPortraitAsset: {
+        hlsVideoUrl: "https://experience-media.dolby.com/Dolby.com/20231227/Love_More/Ed_Sheeran/SDRS/Portrait/unencrypted/hls/LM_ES-Destination_SDR-9x16_A-Binaural_vs_Stereo_SDRS_manifest-priority.m3u8",
+        mpegDashVideoUrl: "https://experience-media.dolby.com/Dolby.com/20231227/Love_More/Ed_Sheeran/SDRS/Portrait/unencrypted/dash/LM_ES-Destination_SDR-9x16_A-Binaural_vs_Stereo_SDRS_manifest-priority.mpd"
+      },
+      dolbyOffLandscapeAsset: {
+        hlsVideoUrl: "https://experience-media.dolby.com/Dolby.com/20231227/Love_More/Ed_Sheeran/SDRS/Landscape/unencrypted/hls/LM_ES-Destination_SDR_A-Binaural_vs_Stereo_SDRS_manifest-priority.m3u8",
+        mpegDashVideoUrl: "https://experience-media.dolby.com/Dolby.com/20231227/Love_More/Ed_Sheeran/SDRS/Landscape/unencrypted/dash/LM_ES-Destination_SDR_A-Binaural_vs_Stereo_SDRS_manifest-priority.mpd"
+      }
+    }
 
-          const { onOffDemoItemBlocks } = resp.body;
-          if (!onOffDemoItemBlocks) {
-            return resp
-          }
+    const { onOffDemoItemBlocks } = resp.body;
+    if (!onOffDemoItemBlocks) {
+      return resp
+    }
 
-          // Access existing data for replacement target
-          const targetIndex = onOffDemoItemBlocks.findIndex(b => b.id === targetBlockId)
-          const targetBlock = onOffDemoItemBlocks[targetIndex];
+    // Access existing data for replacement target
+    const targetIndex = onOffDemoItemBlocks.findIndex(b => b.id === targetBlockId)
+    const targetBlock = onOffDemoItemBlocks[targetIndex];
 
-          // Get asset default data
-          const landscapeData = targetBlock.dolbyOffLandscapeAsset || targetBlock.dolbyOnLandscapeAsset;
-          const portraitData = targetBlock.dolbyOffPortraitAsset || targetBlock.dolbyOnPortraitAsset;
+    // Get asset default data
+    const landscapeData = targetBlock.dolbyOffLandscapeAsset || targetBlock.dolbyOnLandscapeAsset;
+    const portraitData = targetBlock.dolbyOffPortraitAsset || targetBlock.dolbyOnPortraitAsset;
 
-          /**
-           * Adding optimizely tokens here might be possible instead of manually
-           * targeting new assets. 
-           */
-          const replacementData = {
-              dolbyOffLandscapeAsset: {
-                ...landscapeData,
-                hlsVideoUrl: replacementAssets.dolbyOffLandscapeAsset.hlsVideoUrl,
-              }, 
-              dolbyOffPortraitAsset: {
-                ...portraitData,
+    /**
+     * Adding optimizely tokens here might be possible instead of manually
+     * targeting new assets. 
+     */
+    const replacementData = {
+        dolbyOffLandscapeAsset: {
+          ...landscapeData,
+          hlsVideoUrl: replacementAssets.dolbyOffLandscapeAsset.hlsVideoUrl,
+        }, 
+        dolbyOffPortraitAsset: {
+          ...portraitData,
 
-              },
-              dolbyOnLandscapeAsset: undefined, // Remove enhanced
-              dolbyOnPortraitAsset: undefined
-          }
+        },
+        dolbyOnLandscapeAsset: undefined, // Remove enhanced
+        dolbyOnPortraitAsset: undefined
+    }
 
-          const newBlocksData = onOffDemoItemBlocks;
-          newBlocksData[targetIndex] = {
-            ...targetBlock,
-            ...replacementData
-          }
+    const newBlocksData = onOffDemoItemBlocks;
+    newBlocksData[targetIndex] = {
+      ...targetBlock,
+      ...replacementData
+    }
 
-          return new Response(JSON.stringify({
-            // Fill in defaults with response data
-            ...resp.body,
-            // Overwrite src's for return
-            onOffDemoItemBlocks: onOffDemoItemBlocks
-          }), { headers: { 'Content-Type': 'application/json'}})
-        }
+    return new Response(JSON.stringify({
+      // Fill in defaults with response data
+      ...resp.body,
+      // Overwrite src's for return
+      onOffDemoItemBlocks: onOffDemoItemBlocks
+    }), { headers: { 'Content-Type': 'application/json'}})
+  }
 
-        return resp;
-      })
-  );
+  return resp;
 })
